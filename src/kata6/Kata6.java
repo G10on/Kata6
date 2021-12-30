b557527d1ecd0e695e3feba4c98299f5988168b5
G10Mini <G10Mini@DESKTOP-4RFEI7P>
2021-12-30 11:58:49 +0100

    Kata6 v3

--- src/kata6/Kata6.java
+++ src/kata6/Kata6.java
@@ -6,6 +6,7 @@ import toys.SerialNumberGenerator;
 import toys.Car;
 import toys.Helicopter;
 import toys.SerialNumberGenerator;
+import toys.ToyBusiness;
 
 public class Kata6 {
     
@@ -13,34 +14,29 @@ public class Kata6 {
         SerialNumberGenerator generator = new SerialNumberGenerator();
         ArrayList<Car> cars = new ArrayList<>();
         ArrayList<Helicopter> helicopters= new ArrayList<>();
-        Scanner in = new Scanner(System.in);
+        ToyBusiness business = new ToyBusiness();
+        Scanner input = new Scanner(System.in);
         String line = "";
 
         while(!line.equals("exit")) {
-            line = in.nextLine();
+            line = input.nextLine();
             switch (line) {
-                case "car": 
-                     Car car = new Car(generator.next());
-                     car.pack();
-                     car.label();
-                     cars.add(car);
-                     System.out.println("Built cars: "+cars.stream().map(c -> c.getSerialNumber().toString()).collect(Collectors.joining(", ")));
-                     break;
+                case "car":
+                    cars.add(business.createCar());
+                    System.out.println("Built cars: "+cars.stream().map(c -> c.getSerialNumber().toString()).collect(Collectors.joining(", ")));
+                    break;
 
-                 case "helicopter": 
-                     Helicopter helicopter = new Helicopter(generator.next());
-                     helicopter.pack();
-                     helicopter.label();
-                     helicopters.add(helicopter);
-                     System.out.println("Built helicopters: "+helicopters.stream().map(c -> c.getSerialNumber().toString()).collect(Collectors.joining(", ")));
-                     break;
+                case "helicopter":
+                    helicopters.add(business.createHelicopter());
+                    System.out.println("Built helicopters: "+helicopters.stream().map(c -> c.getSerialNumber().toString()).collect(Collectors.joining(", ")));
+                    break;
 
-                 case "exit":
-                     System.out.println("Exiting...");
-                     break;
+                case "exit":
+                    System.out.println("Exiting...");
+                    break;
 
-                 default:
-                     System.out.println("Command not found");
+                default:
+                    System.out.println("Command not found");
             }
         }
     }
